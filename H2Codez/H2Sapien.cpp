895b20bf2b35339fcc7f922264a1295e0b6ebad4
num0005 <double.null@outlook.com>
2017-10-24 23:10:40 +0100

    wip

--- H2Codez/H2Sapien.cpp
+++ H2Codez/H2Sapien.cpp
@@ -10,6 +10,22 @@ create_main_window create_main_window_orginal;
 typedef bool(__fastcall *load_main_window)(int thisptr, int unused, int a2, int a3, int a4, int *a5);
 load_main_window load_main_window_orginal;
 
+typedef int (WINAPI *LoadStringW_Typedef)(HINSTANCE hInstance, UINT uID, LPWSTR lpBuffer, int cchBufferMax);
+LoadStringW_Typedef LoadStringW_Orginal;
+
+bool fake;
+int WINAPI LoadStringW_Hook(HINSTANCE hInstance, UINT uID, LPWSTR lpBuffer, int cchBufferMax)
+{
+	if (uID == 421 || uID == 430 || uID == 431 || uID == 633 || uID == 763 || uID == 768 || uID == 4550 || uID == 4690 || uID == 4745) {
+		fake = !fake;
+	}
+
+	if (uID == 4745) {
+		fake = !fake;
+	}
+	return LoadStringW_Orginal(hInstance, uID, lpBuffer, cchBufferMax);
+}
+
 bool __fastcall load_main_window_hook(int thisptr, int unused, int a2, int a3, int a4, int *a5)
 {
 	int menu_ptr = thisptr + 12;
@@ -29,6 +45,9 @@ void H2SapienPatches::Init()
 #pragma region Patches
 	// stop the default menu overwriting our custom one
 	NopFill(0x47AD09, 0x15);
+
+	BYTE patch_can_display_tag[] = { 0xB8, 0x01, 0x00, 0x00, 0x00, 0xC3 }; // mov eax, 1; retn
+	WriteBytesASM(0x630200, patch_can_display_tag, sizeof(patch_can_display_tag));
 #pragma endregion
 
 #pragma region Hooks
@@ -41,6 +60,9 @@ void H2SapienPatches::Init()
 
 	load_main_window_orginal = CAST_PTR(load_main_window,0x47ACE0);
 	DetourAttach(&(PVOID&)load_main_window_orginal, load_main_window_hook);
+
+	LoadStringW_Orginal = LoadStringW;
+	DetourAttach(&(PVOID&)LoadStringW_Orginal, LoadStringW_Hook);
 
 	DetourTransactionCommit();
 #pragma endregion
